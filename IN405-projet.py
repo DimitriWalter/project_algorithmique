16afdb67bbcb1e763ae3abbb98be83e9f08976b4
Dimitri WALTER <dimitri.walter@ens.uvsq.fr>
2024-04-29 18:32:59 +0200

    update

--- IN405-projet.py
+++ IN405-projet.py
@@ -1,223 +1,227 @@
-import random
+import random as rd
 import networkx as nx
 import matplotlib.pyplot as plt
 from collections import deque, defaultdict
 import heapq
 
 # Définition des constantes
-NUM_TIER1_NODES = 10
-NUM_TIER2_NODES = 20
-NUM_TIER3_NODES = 70
-TOTAL_NODES = NUM_TIER1_NODES + NUM_TIER2_NODES + NUM_TIER3_NODES
+nombre_noeud_tier1 = 10
+nombre_noeud_tier2 = 20
+nombre_noeud_tier3 = 70
+nombre_noeud_total = nombre_noeud_tier1 + nombre_noeud_tier2 + nombre_noeud_tier3
 
-class Network:
+
+class Reseau:
     def __init__(self):
-        self.nodes = self.create_network()
-        self.routing_tables = self.calculate_routing_tables()
+        self.noeuds = self.creer_reseau()
+        self.table_routage = self.calculer_table_routage()
 
-    def create_network(self):
-        # Création des nœuds
-        nodes = [{'tier': 1, 'connections': []} for _ in range(NUM_TIER1_NODES)]
-        nodes += [{'tier': 2, 'connections': []} for _ in range(NUM_TIER2_NODES)]
-        nodes += [{'tier': 3, 'connections': []} for _ in range(NUM_TIER3_NODES)]
+    def creer_reseau(self):
+        # Création des noeuds
+        noeuds = [{'tier': 1, 'connections': []} for _ in range(nombre_noeud_tier1)]
+        noeuds += [{'tier': 2, 'connections': []} for _ in range(nombre_noeud_tier2)]
+        noeuds += [{'tier': 3, 'connections': []} for _ in range(nombre_noeud_tier3)]
 
         # Création des liens pour le backbone (Tier 1)
-        for i in range(NUM_TIER1_NODES):
-            for j in range(i + 1, NUM_TIER1_NODES):
-                if random.random() < 0.75:
-                    weight = random.randint(5, 10)
-                    nodes[i]['connections'].append((j, weight))
-                    nodes[j]['connections'].append((i, weight))
+        for i in range(nombre_noeud_tier1):
+            for j in range(i + 1, nombre_noeud_tier1):
+                if rd.random() < 0.75:
+                    poids = rd.randint(5, 10)
+                    noeuds[i]['connections'].append((j, poids))
+                    noeuds[j]['connections'].append((i, poids))
 
         # Création des liens pour les opérateurs de niveau 2 (Tier 2)
-        tier2_start = NUM_TIER1_NODES
-        tier2_end = NUM_TIER1_NODES + NUM_TIER2_NODES
-        for i in range(tier2_start, tier2_end):
-            tier1_connections = random.sample(range(NUM_TIER1_NODES), random.randint(1, 2))
-            for tier1_node in tier1_connections:
-                weight = random.randint(10, 20)
-                nodes[i]['connections'].append((tier1_node, weight))
-                nodes[tier1_node]['connections'].append((i, weight))
+        debut_tier2 = nombre_noeud_tier1
+        fin_tier2 = nombre_noeud_tier1 + nombre_noeud_tier2
+        for i in range(debut_tier2, fin_tier2):
+            connections_tier1 = rd.sample(range(nombre_noeud_tier1), rd.randint(1, 2))
+            for noeud_tier1 in connections_tier1:
+                poids = rd.randint(10, 20)
+                noeuds[i]['connections'].append((noeud_tier1, poids))
+                noeuds[noeud_tier1]['connections'].append((i, poids))
 
-            tier2_connections = random.sample(range(tier2_start, tier2_end), random.randint(2, 3))
-            for tier2_node in tier2_connections:
-                if tier2_node != i:
-                    weight = random.randint(10, 20)
-                    nodes[i]['connections'].append((tier2_node, weight))
-                    nodes[tier2_node]['connections'].append((i, weight))
+            connections_tier2 = rd.sample(range(debut_tier2, fin_tier2), rd.randint(2, 3))
+            for noeud_tier2 in connections_tier2:
+                if noeud_tier2 != i:
+                    poids = rd.randint(10, 20)
+                    noeuds[i]['connections'].append((noeud_tier2, poids))
+                    noeuds[noeud_tier2]['connections'].append((i, poids))
 
         # Création des liens pour les opérateurs de niveau 3 (Tier 3)
-        tier3_start = NUM_TIER1_NODES + NUM_TIER2_NODES
-        for i in range(tier3_start, TOTAL_NODES):
-            tier2_connections = random.sample(range(tier2_start, tier2_end), 2)
-            for tier2_node in tier2_connections:
-                weight = random.randint(20, 50)
-                nodes[i]['connections'].append((tier2_node, weight))
-                nodes[tier2_node]['connections'].append((i, weight))
+        debut_tier3 = nombre_noeud_tier1 + nombre_noeud_tier2
+        for i in range(debut_tier3, nombre_noeud_total):
+            connections_tier2 = rd.sample(range(debut_tier2, fin_tier2), 2)
+            for noeud_tier2 in connections_tier2:
+                poids = rd.randint(20, 50)
+                noeuds[i]['connections'].append((noeud_tier2, poids))
+                noeuds[noeud_tier2]['connections'].append((i, poids))
 
-        return nodes
+        return noeuds
 
-    def bfs(self, start_node):
+    def bfs(self, noeud_depart):
         """
-        Parcours en largeur (BFS) pour vérifier si tous les nœuds sont accessibles
-        à partir du nœud de départ.
+        Parcours en largeur (BFS) pour vérifier si tous les noeuds sont accessibles
+        à partir du noeud de départ.
         """
-        visited = set()
-        queue = [(start_node, None)]  # (nœud, nœud parent)
+        visite = set()
+        file = [(noeud_depart, None)]  # (nœud, nœud parent)
 
-        while queue:
-            node, parent = queue.pop(0)
-            if node not in visited:
-                visited.add(node)
-                for neighbor, weight in self.nodes[node]['connections']:
-                    queue.append((neighbor, node))
+        while file:
+            noeud, parent = file.pop(0)
+            if noeud not in visite:
+                visite.add(noeud)
+                for voisin, poids in self.noeuds[noeud]['connections']:
+                    file.append((voisin, noeud))
 
         # Si tous les nœuds ont été visités, le réseau est connexe
-        return len(visited) == len(self.nodes)
+        return len(visite) == len(self.noeuds)
 
-    def dijkstra(self, start_node):
+    def dijkstra(self, noeud_depart):
         """
         Algorithme de Dijkstra pour calculer les plus courts chemins
         à partir d'un nœud source vers tous les autres nœuds.
         """
-        distances = {node: float('inf') for node in range(len(self.nodes))}
-        distances[start_node] = 0
-        pq = [(0, start_node)]
-        prev = {node: None for node in range(len(self.nodes))}
+        distances = {noeud: float('inf') for noeud in range(len(self.noeuds))}
+        distances[noeud_depart] = 0
+        pq = [(0, noeud_depart)]
+        prev = {noeud: None for noeud in range(len(self.noeuds))}
 
         while pq:
-            current_dist, current_node = heapq.heappop(pq)
+            distance_actuelle, noeud_actuel = heapq.heappop(pq)
 
-            if current_dist > distances[current_node]:
+            if distance_actuelle > distances[noeud_actuel]:
                 continue
 
-            for neighbor, weight in self.nodes[current_node]['connections']:
-                distance = current_dist + weight
-                if distance < distances[neighbor]:
-                    distances[neighbor] = distance
-                    prev[neighbor] = current_node
-                    heapq.heappush(pq, (distance, neighbor))
+            for voisin, poids in self.noeuds[noeud_actuel]['connections']:
+                distance = distance_actuelle + poids
+                if distance < distances[voisin]:
+                    distances[voisin] = distance
+                    prev[voisin] = noeud_actuel
+                    heapq.heappush(pq, (distance, voisin))
 
         return prev, distances
 
-    def calculate_routing_tables(self):
+    def calculer_table_routage(self):
         """
-        Calcule les tables de routage pour chaque nœud en utilisant l'algorithme de Dijkstra.
+        Calcule les tables de routage pour chaque noeud en utilisant l'algorithme de Dijkstra.
         """
-        routing_tables = [{} for _ in range(len(self.nodes))]
+        table_routage = [{} for _ in range(len(self.noeuds))]
 
-        for node in range(len(self.nodes)):
-            prev, distances = self.dijkstra(node)
-            for dest in range(len(self.nodes)):
-                if dest != node and distances[dest] != float('inf'):
-                    next_hop = prev[dest]
-                    routing_tables[node][dest] = next_hop
+        for noeud in range(len(self.noeuds)):
+            prev, distances = self.dijkstra(noeud)
+            for destination in range(len(self.noeuds)):
+                if destination != noeud and distances[destination] != float('inf'):
+                    prochain_saut = prev[destination]
+                    table_routage[noeud][destination] = prochain_saut
 
-        return routing_tables
+        return table_routage
 
-    def reconstruct_path(self, source, destination):
+    def reconstruire_chemin(self, source, destination):
         """
-        Reconstitue le chemin entre deux nœuds en utilisant les tables de routage.
+        Reconstitue le chemin entre deux noeuds en utilisant les tables de routage
         """
-        path = [destination]
-        current_node = destination
-        while current_node != source:
-            next_hop = self.routing_tables[source][current_node]
-            path.append(next_hop)
-            current_node = next_hop
-        path.reverse()
-        return path
+        chemin = [destination]
+        noeud_actuel = destination
+        while noeud_actuel != source:
+            prochain_saut = self.table_routage[source][noeud_actuel]
+            chemin.append(prochain_saut)
+            noeud_actuel = prochain_saut
+        chemin.reverse()
+        return chemin
 
-class VisualNetwork(Network):
+
+class Reseau_graphique(Reseau):
     def __init__(self):
         super().__init__()
 
-    def visualize(self):
+    def afficher(self):
         G = nx.Graph()
         positions = {}
 
-        for node_idx, node in enumerate(self.nodes):
-            G.add_node(node_idx, size=250 if node['tier'] == 1 else 200 if node['tier'] == 2 else 150)
-            positions[node_idx] = (random.random(), random.random())
+        for noeud_idx, noeud in enumerate(self.noeuds):
+            G.add_node(noeud_idx, size=250 if noeud['tier'] == 1 else 200 if noeud['tier'] == 2 else 150)
+            positions[noeud_idx] = (rd.random(), rd.random())
 
-            for neighbor, weight in node['connections']:
-                G.add_edge(node_idx, neighbor, weight=weight)
+            for voisin, poids in noeud['connections']:
+                G.add_edge(noeud_idx, voisin, poids=poids)
 
-        node_colors = ['blue' if self.nodes[node]['tier'] == 1 else 'green' if self.nodes[node]['tier'] == 2 else 'orange' for node in G.nodes()]
-        node_sizes = [G.nodes[node]['size'] for node in G.nodes()]
-        nx.draw(G, pos=positions, with_labels=True, node_color=node_colors, node_size=node_sizes)
+        couleurs_noeuds = ['blue' if self.noeuds[noeud]['tier'] == 1 else 'green' if self.noeuds[noeud]['tier'] == 2 else 'orange' for noeud in G.nodes()]
+        taille_noeuds = [G.nodes[noeud]['size'] for noeud in G.nodes()]
+        nx.draw(G, pos=positions, with_labels=True, node_color=couleurs_noeuds, node_size=taille_noeuds)
 
         # Ajuster l'épaisseur des liens en fonction de la valeur
-        edge_widths = [0.5 + 0.2 * (G[u][v]['weight'] - 5) / 5 for u, v in G.edges()]
-        nx.draw_networkx_edges(G, pos=positions, width=edge_widths)
+        largeur_bordures = [0.5 + 0.2 * (G[u][v]['poids'] - 5) / 5 for u, v in G.edges()]
+        nx.draw_networkx_edges(G, pos=positions, width=largeur_bordures)
 
         plt.legend([plt.Line2D([0], [0], color='blue', lw=4), plt.Line2D([0], [0], color='green', lw=4), plt.Line2D([0], [0], color='orange', lw=4)],
                    ['Tier 1', 'Tier 2', 'Tier 3'], loc='upper left')
 
         plt.show()
 
-    def select_nodes(self):
+    def selectionner_noeuds(self):
         while True:
             try:
-                source_node = int(input("Enter the source node: "))
-                if 0 <= source_node < len(self.nodes):
+                source_noeud = int(input("Entrez le noeud source : "))
+                if 0 <= source_noeud < len(self.noeuds):
                     break
                 else:
-                    print("Source node out of range. Please enter a valid node.")
+                    print("Noeud source non existant, entrez un noeud du graphe")
             except ValueError:
-                print("Invalid input. Please enter an integer.")
+                print("Noeud source incorrect, entrez un entier")
 
         while True:
             try:
-                dest_node = int(input("Enter the destination node: "))
-                if 0 <= dest_node < len(self.nodes):
+                destination_noeud = int(input("Entrez le noeud de destination : "))
+                if 0 <= destination_noeud < len(self.noeuds):
                     break
                 else:
-                    print("Destination node out of range. Please enter a valid node.")
+                    print("Noeud destination non existant, entrez un noeud du graphe")
             except ValueError:
-                print("Invalid input. Please enter an integer.")
+                print("Noeud destination incorrect, entrez un entier")
 
-        return source_node, dest_node
+        return source_noeud, destination_noeud
 
-    def visualize_path(self, path):
+    def afficher_chemin(self, chemin):
         G = nx.Graph()
         positions = {}
 
-        for node_idx, node in enumerate(self.nodes):
-            G.add_node(node_idx, size=250 if node['tier'] == 1 else 200 if node['tier'] == 2 else 150)
-            positions[node_idx] = (random.random(), random.random())
+        for noeud_idx, noeud in enumerate(self.noeuds):
+            G.add_node(noeud_idx, size=250 if noeud['tier'] == 1 else 200 if noeud['tier'] == 2 else 150)
+            positions[noeud_idx] = (rd.random(), rd.random())
 
-            for neighbor, weight in node['connections']:
-                G.add_edge(node_idx, neighbor, weight=weight)
+            for voisin, poids in noeud['connections']:
+                G.add_edge(noeud_idx, voisin, poids=poids)
 
-        edges = [(path[i], path[i+1]) for i in range(len(path)-1)]
-        node_colors = ['blue' if self.nodes[node]['tier'] == 1 else 'green' if self.nodes[node]['tier'] == 2 else 'orange' for node in G.nodes()]
-        node_sizes = [G.nodes[node]['size'] for node in G.nodes()]
-        nx.draw(G, pos=positions, with_labels=True, node_color=node_colors, node_size=node_sizes)
+        bordures = [(chemin[i], chemin[i+1]) for i in range(len(chemin)-1)]
+        couleurs_noeuds = ['blue' if self.noeuds[noeud]['tier'] == 1 else 'green' if self.noeuds[noeud]['tier'] == 2 else 'orange' for noeud in G.nodes()]
+        taille_noeuds = [G.nodes[noeud]['size'] for noeud in G.nodes()]
+        nx.draw(G, pos=positions, with_labels=True, node_color=couleurs_noeuds, node_size=taille_noeuds)
 
         # Ajuster l'épaisseur des liens en fonction de la valeur
-        edge_widths = [0.5 + 0.2 * (G[u][v]['weight'] - 5) / 5 for u, v in G.edges()]
-        nx.draw_networkx_edges(G, pos=positions, width=edge_widths)
+        largeur_bordures = [0.5 + 0.2 * (G[u][v]['poids'] - 5) / 5 for u, v in G.edges()]
+        nx.draw_networkx_edges(G, pos=positions, width=largeur_bordures)
 
         # Épaisseur du chemin sélectionné
-        path_width = 3
-        nx.draw_networkx_edges(G, pos=positions, edgelist=edges, edge_color='r', width=path_width)
+        largeur_chemin = 3
+        nx.draw_networkx_edges(G, pos=positions, edgelist=bordures, edge_color='r', width=largeur_chemin)
 
         plt.legend([plt.Line2D([0], [0], color='blue', lw=4), plt.Line2D([0], [0], color='green', lw=4), plt.Line2D([0], [0], color='orange', lw=4)],
                    ['Tier 1', 'Tier 2', 'Tier 3'], loc='upper left')
 
         plt.show()
 
+
 def main():
-    visual_network = VisualNetwork()
-    visual_network.visualize()
+    le_reseau_graphique = Reseau_graphique()
+    le_reseau_graphique.afficher()
 
-    # Select source and destination nodes
-    source_node, dest_node = visual_network.select_nodes()
+    # Selectionne le noeud source et destination
+    source_noeud, destination_noeud = le_reseau_graphique.selectionner_noeuds()
 
-    # Reconstruct and visualize the path
-    path = visual_network.reconstruct_path(source_node, dest_node)
-    visual_network.visualize_path(path)
+    # Reconstruit et affiche le chemin
+    chemin = le_reseau_graphique.reconstruire_chemin(source_noeud, destination_noeud)
+    le_reseau_graphique.afficher_chemin(chemin)
+
 
 if __name__ == "__main__":
     main()
